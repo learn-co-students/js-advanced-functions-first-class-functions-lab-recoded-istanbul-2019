17634ac7f1639c70b57aa833af008ca76c5ab962
mmelihdogan <doganmmelih@gmail.com>
2020-02-13 17:28:30 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
