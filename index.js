1d5b0d8985a6b22e2181aa99407437f6dd3fff33
emrerdem1 <emrerdem94@gmail.com>
2020-05-03 00:18:03 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+const returnFirstTwoDrivers = (drivers) => {
+    let [first, second] = drivers;
+    let newArr = [first, second];
+    return newArr;
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    let [, , ltwo, lone] = drivers;
+    let newArr = [ltwo, lone];
+    return newArr;
+}
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+/*const createFareMultiplier = (int) => {
+    const fareMultiplier = (int) => int * int;
+    const fareQuintupler = (int) => int * 5;
+    const fareDoubler = (int) => int * 2;
+    const fareTripler = (int) => int * 3;
+    return fareMultiplier
+}*/
+const createFareMultiplier = (int) => {
+    fareMultiplier(int);
+    fareQuintupler(int);
+    fareDoubler(int);
+    fareTripler(int);
+    return fareMultiplier;
+
+}
+const fareMultiplier = (int) => int * int;
+const fareQuintupler = (int) => int * 5;
+const fareDoubler = (int) => int * 2;
+const fareTripler = (int) => int * 3;
+
+const selectDifferentDrivers = (drivers, func) => func(drivers)
+
